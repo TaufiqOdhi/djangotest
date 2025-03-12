7a575f4e9249a56d5f756450302f3fe32d94926c
TaufiqOdhi <taufiq.odhi@gmail.com>
2025-03-13 05:55:09 +0700

    fix seeder data and add script for load seeder data

---
+++ scripts/load_seeder_data.sh
@@ -0,0 +1,4 @@
+python manage.py loaddata seeder/group.json
+python manage.py loaddata seeder/user.json
+python manage.py loaddata seeder/modular_engine.json
+python manage.py loaddata seeder/module_product.json
